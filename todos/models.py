42112327403b8951694aa9dcd5f705bd83e856fe
ikizey <ikizey@gmail.com>
2020-07-26 02:08:28 +0300

    added todos model| pre migration

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Todos(models.Model):
+
+    todo = models.CharField("TODO", max_length=100)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    user = models.ForeignKey(get_user_model(),
+                             on_delete=models.CASCADE,
+                             related_name='user')
+
+    def __str__(self):
+        return self.todo
