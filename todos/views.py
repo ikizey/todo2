b38caee5a193eb6b03e2c708fc4498bb2471ac4c
ikizey <ikizey@gmail.com>
2020-07-26 20:27:27 +0300

    added todos formset

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,22 @@
+from django.forms import modelformset_factory
 from django.shortcuts import render
+
+from .models import Todos
 
 
 def hello(request):
-    return render(request, "todos/hello.html")
+
+    template = "todos/hello.html"
+
+    FormSet = modelformset_factory(Todos, fields=['id', 'todo'], extra=5)
+    context = {'form': FormSet()}
+
+    if request.method == 'POST':
+        form = FormSet(request.POST)
+        if form.is_valid():
+            instances = form.save(commit=False)
+            for instance in instances:
+                instance.user = request.user
+                instance.save()
+
+    return render(request, template, context=context)
