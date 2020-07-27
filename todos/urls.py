229fba7d7e4d7dfef31ffdc0f60bafbeb5ad3a9f
ikizey <ikizey@gmail.com>
2020-07-27 17:55:42 +0300

    changed formset, added todo url routing

--- todos/urls.py
+++ todos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello, name="hello")
+    path('', views.hello, name="hello"),
+    path('todos/<str:user_name>/', views.todo, name="todo"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,22 +1,37 @@
-from django.forms import modelformset_factory
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
+from django.forms import inlineformset_factory
+from django.shortcuts import redirect, render
 
 from .models import Todos
 
 
 def hello(request):
+    try:
+        get_user_model().objects.get(username=str(request.user))
+    except ObjectDoesNotExist:
+        return redirect('login')
+
+    return redirect('todo', str(request.user))
+
+
+def todo(request, user_name):
+    if str(request.user) != user_name:
+        return redirect('hello')
 
     template = "todos/hello.html"
 
-    FormSet = modelformset_factory(Todos, fields=['id', 'todo'], extra=5)
-    context = {'form': FormSet()}
+    user = get_user_model().objects.get(username=user_name)
+
+    FormSet = inlineformset_factory(
+        get_user_model(), Todos, fields=('todo',), extra=5)
 
     if request.method == 'POST':
-        form = FormSet(request.POST)
-        if form.is_valid():
-            instances = form.save(commit=False)
-            for instance in instances:
-                instance.user = request.user
-                instance.save()
+        forms = FormSet(request.POST, instance=user)
+        if forms.is_valid():
+            forms.save()
+        return redirect('hello', user_name=user_name)
 
+    forms = FormSet(instance=user)
+    context = {'form': forms}
     return render(request, template, context=context)
